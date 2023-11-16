39aa0abc692ec0fdeee8facfaf8c911f2dc1a0b8
otisdev95 <olaliotis95@gmail.com>
2023-11-16 04:58:34 +0100

    updated boiler-project-exercisetracker

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express')
+const bodyPaser = require('body-parser')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyPaser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+const users = [];
+
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+ 
+  const newUser = {
+    username: username,
+    _id: generateUserId()
+  }
+
+users.push(newUser);
+
+
+  return res.json({
+    username: newUser.username,
+    _id: newUser._id
+  });
 });
 
 
